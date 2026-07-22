fd464ababe802df6fa5be78f7bdce3661a73d8ad
David Monteiro <dmonn97@gmail.com>
2026-07-22 10:57:47 -0300

    make build

---
+++ layout_landing-page.f75de5e1.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+});
+
+//# sourceMappingURL=layout_landing-page.f75de5e1.js.map
